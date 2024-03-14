552a009733c1c066c7ea40560204e23578c9911a
Jose Ignacio Ojeda <jojedava@gmail.com>
2024-03-14 08:27:19 +0100

    Add reports

--- appium-training/src/test/java/TestRunner.java
+++ appium-training/src/test/java/TestRunner.java
@@ -4,7 +4,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = "steps")
+@CucumberOptions(features = "src/test/resources/features", glue = "steps", plugin = { "pretty",
+        "html:target/cucumber-reports.html" })
 public class TestRunner {
 
     // @AfterClass
--- appium-training/target/test-classes/features/ios.feature
+++
@@ -1,7 +0,0 @@
-Feature: iOS Appium Example
-
-  Scenario: Compute Sum
-    Given the user adds the first number 4
-    And the user adds the second number 4
-    When the user clicks on the button
-    Then the app show the operation result 8
